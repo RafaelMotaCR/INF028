20f308ab171081f0c50366f403b1ceb838d9ebc3
RafaelMotaCR <128419553+RafaelMotaCR@users.noreply.github.com>
2023-11-15 01:43:34 +0000

    Minor changes.

--- testes/binaryTree.c
+++ testes/binaryTree.c
@@ -1,73 +1,187 @@
-# include <stdio.h>
+# include <stdio.h> 
 # include <stdlib.h>
 
-typedef struct Node{
-  int key;
-  struct Node * left;
-  struct Node * right;
-  struct Node * m;
-} Node;
-
-typedef struct Tree{
-  Node * root;
-}Tree;
+typedef struct node{
+    struct node * left;
+    struct node * right;
+    struct node * father;
+    int key;
+} node;
 
 
-Node * create_node(int Key){
-  Node * node = (Node*)malloc(sizeof(node));
-  node -> key = Key;
-  return node;
+typedef struct tree{
+    node * root;
+} tree;
+
+
+node * createNode(int k){
+    node * tmp = (node *) malloc(sizeof(node));
+    if(tmp != NULL){
+        tmp -> key = k;
+    }
+    return tmp;
+
 }
 
 
-void inorder_tree_walk(Node * x){
-  if(x != NULL){
-    inorder_tree_walk(x->left);
-    printf(" %d ", x -> key);
-    inorder_tree_walk(x->right);
-  }
-}
+void treeInsert(tree * T, int k){
 
+    node * newNode = createNode(k);
 
-void tree_insert(Tree * T, Node * z){
-  Node * y = NULL;
-  Node * x = T -> root;
-  while(x != NULL){
-    y = x;
-    if(z -> key < x -> key){
-      x = x -> left;
+    node * x = T -> root;
+    node * y = NULL;
+
+    while(x != NULL){
+
+        y = x;
+
+        if(newNode -> key > x -> key){
+            x = x -> right;
+        }
+        else{
+            x = x -> left;
+        }
+
+    }
+
+    if(y == NULL){
+        T -> root = newNode;
+    }
+    else if(newNode -> key > y -> key){
+        y -> right = newNode;
     }
     else{
-      x = x -> right;
+        y -> left = newNode;
     }
-  }
-  x -> m = y;
-  if(y == NULL){
-    T -> root = z;
-  }
-  else if(z -> key < y -> key){
-    y -> left = x;
-  }
-  else{
-    y -> right = z;
-  }
-}
-int main(){
-  Tree * years = malloc(sizeof(Tree));
-  Node * n1 = create_node(1960);
-  Node * n2 = create_node(2001);
-  Node * n3 = create_node(1939);
-  years -> root = n1;
-  n1 -> m = NULL;
-  n1 -> left = n3;
-  n1 -> right = n2;
-  n2 -> m = n1;
-  n2 -> left = NULL;
-  n2 -> left = NULL;
-  n3 -> m = n1;
-  n3 -> left = NULL;
-  n3 -> left = NULL;
-  inorder_tree_walk(years -> root);
-  return EXIT_SUCCESS;
+
 }
 
+
+node * treeSearch(node * x, int k){
+    if(x == NULL  || x -> key == k){
+        return x;
+    }
+    if(k < x -> key){
+        return treeSearch(x -> left, k);
+    }
+    else{
+        return treeSearch(x -> right, k);
+    }
+
+}
+
+
+node * treeMinimum(node * x){
+    if(x -> left == NULL) {
+        return x;
+    }
+    else{  
+        return treeMinimum (x->left);
+    }
+}
+
+
+void printTree(node * root, int space) {
+    // Base case
+    if (root == NULL) {
+        return;
+    }
+
+    // Increase distance between levels
+    space += 5;
+
+    // Process right child first
+    printTree(root->right, space);
+
+    // Print current node after space
+    printf("\n");
+    for (int i = 5; i < space; i++) {
+        printf(" ");
+    }
+    printf("%d\n", root->key);
+
+    // Process left child
+    printTree(root->left, space);
+}
+
+
+void inorderTreeWalk(node * x){
+
+    if(x != NULL){
+
+        inorderTreeWalk(x -> left);
+        printf(" %d ", x -> key);
+        inorderTreeWalk(x -> right);
+
+    }
+
+}
+
+
+node * treeSuccessor(node * x){
+    if(x -> right != NULL){
+        return treeMinimum(x -> right);
+    }
+    node * y = x -> father; 
+    while(y != NULL  && x == y -> right){
+        x = y;
+        y = y -> father;
+    }
+    return y;
+}
+
+
+void treeDelete(tree * T, int k){ 
+    node * z = treeSearch(T -> root, k);
+    node * y;
+    node * x;
+
+    if(z -> left == NULL || z -> right == NULL){
+        y = z; 
+    }  
+    else{
+        y = treeSuccessor(z);  
+    } 
+
+    if(y -> left != NULL){
+        x = y -> left; 
+    } 
+    else{
+        x = y -> right; 
+    } 
+
+    if(x!=NULL){
+        x -> father = y -> father; 
+    } 
+
+    if(y -> father == NULL){
+      T -> root = x;
+    } 
+    else{
+        if(y == y -> father -> left){
+            y -> father -> left =  x;
+
+        } 
+        else{
+            y -> father -> right = x; 
+        }
+    }
+    if(y != z){
+        z -> key = y -> key;
+    } 
+    free(y);
+}
+
+
+int main(){
+    tree * T = (tree*) malloc(sizeof(tree));
+    int input = 1;
+    while (input)
+    {
+        printf("> Enter a num: ");
+        scanf("%d", & input);
+        treeInsert(T, input);
+    }
+
+    printTree(T -> root, 2);
+}
