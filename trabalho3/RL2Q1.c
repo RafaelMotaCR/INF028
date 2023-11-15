ebcfb96cb3f7b7a6cfa17ad599ec2767ae0f432b
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-15 10:44:31 -0300

    Advancement in question 1 of work 3.

--- trabalho3/RL2Q1.c
+++ trabalho3/RL2Q1.c
@@ -14,7 +14,15 @@ typedef struct node
 typedef struct tree
 {
     node * root;
-};
+} tree;
+
+
+tree * initTree()
+{
+    tree * T = (tree*) malloc(sizeof(tree));
+    T -> root = NULL;
+    return T;
+}
 
 
 node * createNode(int k)
@@ -23,45 +31,64 @@ node * createNode(int k)
     if(tmp != NULL)
     {
         tmp -> key = k;
+        tmp -> level = 0;
     }
     return tmp;
 }
 
 
-void insertTree(node * root, int k)
+void insertTree(tree * T, int k)
 {
 
-    node * y;
-    node * x = root;
+    node * y = NULL;
+    node * x = T -> root;
     node * new = createNode(k);
+    int treeLvl = 0;
 
-    while(x != NULL){
+    while(x != NULL)
+    {
         y = x;
-        if(k > x -> key){
-            x = x -> left;
-        }
-        else{
+        if(k > x -> key)
+        {
             x = x -> right;
         }
+        else
+        {
+            x = x -> left;
+        }
+        treeLvl = treeLvl + 1;
     }
 
     new -> father = y;
+    new -> level = treeLvl;
 
-    if(y == NULL){
-        root = new;
+    if(y == NULL)
+    {
+        T -> root = new;
     }
-    else if(k > y -> key){
+    else if(k > y -> key)
+    {
         y -> right = new;
     }
-    else{
+    else
+    {
         y -> left = new;
     }
 
-
+    printf("level: %d\n", new -> level);
 
 }
 
-int main(){
 
+int main(){
+    tree * T = initTree();
+    insertTree(T, 5);
+    insertTree(T, 8);
+    insertTree(T, -1);
+    insertTree(T, 4);
+    insertTree(T, 6);
+    insertTree(T, 9);
+    insertTree(T, 11);
+    insertTree(T, 15);
 
 }
