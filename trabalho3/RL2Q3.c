dcb65e24390e2c130250d3bbf1552de777c378c2
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-16 11:46:47 -0300

    createNode and initTree functions added on work 3 question 3.

--- trabalho3/RL2Q3.c
+++ trabalho3/RL2Q3.c
@@ -2,7 +2,54 @@
 # include <stdlib.h>
 # include <string.h>
 
+typedef struct node
+{
+    struct node * left;
+    struct node * right;
+    struct node * father;
+    int key;
+    int level;
+} node;
 
+
+typedef struct tree
+{
+    node * root;
+} tree;
+
+
+node * createNode(int k)
+{
+    node * tmp = (node*) malloc(sizeof(node));
+
+    if(tmp!= NULL)
+    {
+        tmp -> father = NULL;
+        tmp -> left = NULL;
+        tmp -> right = NULL;
+        tmp -> key = k;
+        tmp -> level = 0;
+    }
+
+    return tmp;
+
+}
+
+
+tree * initTree()
+{
+
+    tree * T = (tree*) malloc(sizeof(tree));
+    T -> root = NULL;
+    return T;
+    
+}
+
+node * insertTree(tree * T, int k){
+
+
+
+}
 
 int main(){
 
