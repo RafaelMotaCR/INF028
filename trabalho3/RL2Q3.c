f3fa083986d0ac338725b16f354773f47f2fd5a0
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-16 12:16:37 -0300

    Tree Minimum and insert tree functions were added.

--- trabalho3/RL2Q3.c
+++ trabalho3/RL2Q3.c
@@ -42,14 +42,60 @@ tree * initTree()
     tree * T = (tree*) malloc(sizeof(tree));
     T -> root = NULL;
     return T;
+
+}
+
+node * insertTree(tree * T, int k)
+{
+
+    node * x = T -> root;
+    node * y = NULL;
+    node * new = createNode(k);
+    int level = 0;
+
+    while(x != NULL)
+    {
+        y = x;
+        level = level + 1;
+        if(new -> key >= x -> key)
+        {
+            x = x -> right;
+        }
+        else
+        {
+            x = x -> left;
+        }
+    }
+
+    new -> father = y;
+    new -> level = level;
+
+    if(y == NULL)
+    {
+        T -> root = new;
+    }
+    else if(new -> key > y -> key)
+    {
+        y -> right = new;
+    }
+    else
+    {
+        y -> left = new;
+    }
+}
+
+
+node * treeMinimum(node * x)
+{
+
+    if(x != NULL)
+    {
+        treeMinimum(x -> left);
+    }
     
+    return x;
 }
 
-node * insertTree(tree * T, int k){
-
-
-
-}
 
 int main(){
 
