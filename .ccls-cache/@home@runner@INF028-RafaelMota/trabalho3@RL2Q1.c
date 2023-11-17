a45c9b1d99feb4b882e02098c0da03046191c3bd
RafaelMotaCR <128419553+RafaelMotaCR@users.noreply.github.com>
2023-11-17 00:32:08 +0000

    Minor changes in several questions.

--- .ccls-cache/@home@runner@INF028-RafaelMota/trabalho3@RL2Q1.c
+++ .ccls-cache/@home@runner@INF028-RafaelMota/trabalho3@RL2Q1.c
@@ -1,187 +1,277 @@
-# include <stdio.h> 
+# include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# define lineMaxSize 500
 
-typedef struct node{
-    struct node * left;
+typedef struct node
+{
     struct node * right;
+    struct node * left;
     struct node * father;
     int key;
+    int level;
 } node;
 
 
-typedef struct tree{
+typedef struct tree
+{
     node * root;
 } tree;
 
 
-node * createNode(int k){
-    node * tmp = (node *) malloc(sizeof(node));
-    if(tmp != NULL){
-        tmp -> key = k;
+// Function prototypes
+
+node * createNode(int k);
+int fileCreate (char * path);
+int fileExists(char * path);
+void freeTree(node * root);
+tree * initTree();
+node * insertTree(tree * T, int k);
+void saveToFile(FILE * fOut, node * n, int isLast);
+
+// Main function
+
+int main()
+{
+    // Checking if both (input and output) files exists
+
+    if(!fileExists("L2Q1.in"))
+    {
+        printf("> Creeper, ohh man!\n");
+        return EXIT_FAILURE;
+
     }
+
+    if(!fileExists("L2Q1.out"))
+    {
+
+        fileCreate("L2Q1.out");
+
+    }
+
+    // If both files exists, open them 
+
+    FILE * fileIn = fopen("L2Q1.in", "r");
+    FILE * fileOut = fopen("L2Q1.out", "w");
+
+    // Uses " " (aka blank space) as separator
+    // Uses line to read each line of the input
+    // Uses "slice" to "broke" line in minor parts, separeted by "separator"
+
+    char * separator = " ";
+    char * slice;
+    char * line = (char*) malloc(lineMaxSize * sizeof(char));
+
+    // Main tree    
+    tree * T = initTree();
+
+    // Used to store the biggest num in the tree
+    node * theBigOne; 
+
+    // Used to temporaly store int numbers 
+    int x; 
+
+    fgets(line, lineMaxSize, fileIn);
+    
+    while(line != NULL)
+    {
+
+        /* Catches the first element, define them as the bigger
+        and inserts it into the tree */
+
+        slice = strtok(line, separator);
+        x = atoi(slice);
+        node * tmp = insertTree(T, x);
+        fprintf(fileOut,"%d", tmp -> level);
+        theBigOne = tmp;
+
+        // Then, catches the next element
+
+        slice = strtok(NULL, separator);
+
+        while (slice != NULL)
+        {
+
+            fputc(32, fileOut); // Inserts a blank space between numbers in output
+            int x = atoi(slice);
+            tmp = insertTree(T, x); 
+
+            /* If any number is greater than the "biggest" 
+            Then it becomes the new "biggest" */
+
+            if(tmp -> key > theBigOne -> key)
+            {
+                theBigOne = tmp;
+            }
+
+            // Saves the level of the last inserted number on the output file
+
+            fprintf(fileOut,"%d", tmp -> level);
+
+            // Goes to the next number
+
+            slice = strtok(NULL, separator);
+
+        }
+
+        /*Tests whether the end of the file has been reached.
+        If not, restart the tree and save the 
+        data from the highest number.*/
+
+        if(fgets(line, lineMaxSize, fileIn) != NULL)
+        {
+
+            saveToFile(fileOut, theBigOne, 0);
+            freeTree(T -> root);
+            theBigOne = NULL;
+            T = initTree();
+
+        }
+        else{
+
+            saveToFile(fileOut, theBigOne, 1);
+            freeTree(T -> root);
+            break;
+
+        }
+
+    }
+
+    fclose(fileIn);
+    fclose(fileOut);
+    free(T);
+    free(line);
+    return EXIT_SUCCESS;
+
+}
+
+// Functions
+
+node * createNode(int k)
+{
+    node * tmp = (node *) malloc(sizeof(node));
+
+    if(tmp != NULL)
+    {
+        tmp -> key = k;
+        tmp -> level = 0;
+        tmp -> right = NULL;
+        tmp -> left = NULL;
+        tmp -> father = NULL;
+    }
+
     return tmp;
 
 }
 
 
-void treeInsert(tree * T, int k){
+int fileCreate (char * path) 
+{
+  FILE * fileTest;
+  fileTest = fopen(path, "w");
 
-    node * newNode = createNode(k);
+  if(fileTest == NULL) 
+  {
+    return 0;
+  }
 
-    node * x = T -> root;
-    node * y = NULL;
-
-    while(x != NULL){
-
-        y = x;
-
-        if(newNode -> key > x -> key){
-            x = x -> right;
-        }
-        else{
-            x = x -> left;
-        }
-
-    }
-
-    if(y == NULL){
-        T -> root = newNode;
-    }
-    else if(newNode -> key > y -> key){
-        y -> right = newNode;
-    }
-    else{
-        y -> left = newNode;
-    }
-
+  fclose(fileTest);
+  return 1;
 }
 
 
-node * treeSearch(node * x, int k){
-    if(x == NULL  || x -> key == k){
-        return x;
-    }
-    if(k < x -> key){
-        return treeSearch(x -> left, k);
-    }
-    else{
-        return treeSearch(x -> right, k);
-    }
+int fileExists(char * path) 
+{
+  FILE * fileTest;
+  fileTest = fopen(path, "r");
 
+  if(fileTest == NULL) 
+  {
+    return 0;
+  }
+
+  fclose(fileTest);
+  return 1;
 }
 
 
-node * treeMinimum(node * x){
-    if(x -> left == NULL) {
-        return x;
-    }
-    else{  
-        return treeMinimum (x->left);
-    }
-}
+void freeTree(node * root) {
 
-
-void printTree(node * root, int space) {
-    // Base case
     if (root == NULL) {
         return;
     }
 
-    // Increase distance between levels
-    space += 5;
-
-    // Process right child first
-    printTree(root->right, space);
-
-    // Print current node after space
-    printf("\n");
-    for (int i = 5; i < space; i++) {
-        printf(" ");
-    }
-    printf("%d\n", root->key);
-
-    // Process left child
-    printTree(root->left, space);
+    freeTree(root -> left);
+    freeTree(root -> right);
+    free(root);
 }
 
 
-void inorderTreeWalk(node * x){
-
-    if(x != NULL){
-
-        inorderTreeWalk(x -> left);
-        printf(" %d ", x -> key);
-        inorderTreeWalk(x -> right);
-
-    }
-
-}
-
-
-node * treeSuccessor(node * x){
-    if(x -> right != NULL){
-        return treeMinimum(x -> right);
-    }
-    node * y = x -> father; 
-    while(y != NULL  && x == y -> right){
-        x = y;
-        y = y -> father;
-    }
-    return y;
-}
-
-
-void treeDelete(tree * T, int k){ 
-    node * z = treeSearch(T -> root, k);
-    node * y;
-    node * x;
-
-    if(z -> left == NULL || z -> right == NULL){
-        y = z; 
-    }  
-    else{
-        y = treeSuccessor(z);  
-    } 
-
-    if(y -> left != NULL){
-        x = y -> left; 
-    } 
-    else{
-        x = y -> right; 
-    } 
-
-    if(x!=NULL){
-        x -> father = y -> father; 
-    } 
-
-    if(y -> father == NULL){
-      T -> root = x;
-    } 
-    else{
-        if(y == y -> father -> left){
-            y -> father -> left =  x;
-
-        } 
-        else{
-            y -> father -> right = x; 
-        }
-    }
-    if(y != z){
-        z -> key = y -> key;
-    } 
-    free(y);
-}
-
-
-int main(){
+tree * initTree()
+{
     tree * T = (tree*) malloc(sizeof(tree));
-    int input = 1;
-    while (input)
+    T -> root = NULL;
+    return T;
+}
+
+
+node * insertTree(tree * T, int k)
+{
+
+    node * y = NULL;
+    node * x = T -> root;
+    node * new = createNode(k);
+    int treeLvl = 0;
+
+    while(x != NULL)
     {
-        printf("> Enter a num: ");
-        scanf("%d", & input);
-        treeInsert(T, input);
+        y = x;
+        if(k >= x -> key)
+        {
+            x = x -> right;
+        }
+        else
+        {
+            x = x -> left;
+        }
+        treeLvl = treeLvl + 1;
     }
 
-    printTree(T -> root, 2);
+    new -> father = y;
+    new -> level = treeLvl;
+
+    if(y == NULL)
+    {
+        T -> root = new;
+    }
+    else if(k > y -> key)
+    {
+        y -> right = new;
+    }
+    else
+    {
+        y -> left = new;
+    }
+
+   return new;
+
+}
+
+
+void saveToFile(FILE * fOut, node * n, int isLast)
+{
+    if(n -> father != NULL)
+    {
+        fprintf(fOut, " max %d alt %d pred %d", n -> key, n -> level, n -> father -> key);
+    }
+    else
+    {
+        fprintf(fOut, " max %d alt %d pred NaN", n -> key, n -> level);
+    }
+
+    if(!isLast)
+    {
+        fputc('\n', fOut);
+    }
+
 }
